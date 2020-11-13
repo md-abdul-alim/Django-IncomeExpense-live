f4b5732b0bbb1718831a42acea79c68dcc041162
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2020-11-13 17:04:54 +0600

    Update settings

--- incomeexpenses/settings.py
+++ incomeexpenses/settings.py
@@ -160,12 +160,12 @@ MESSAGE_TAGS = {
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "ursula.friedman.01@gmail.com"
-EMAIL_HOST_PASSWORD = "ursula.friedman.01@gmail.com"
-DEFAULT_FROM_EMAIL = "51Monirul@thaminA95"
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 
 
